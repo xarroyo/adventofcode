4550b9f1491049c79ba98b189d8974bd08395243
Xavi Arroyo <xavier.arroyo@biteright.dental>
2025-12-03 13:03:02 +0100

    2025/3

---
+++ 2025/adventofcode3/adventofcode3.py
@@ -0,0 +1,24 @@
+
+def read_file_to_list(filename):
+    with open(filename, "r", encoding="utf-8") as file:
+        return [int(line.strip()) for line in file]
+
+def joltage(data):
+    return sum(int(maxbateryjoltage(list(str(v)))) for v in data)
+
+def maxbateryjoltage(digits, nbateries=12):
+    offset= len(digits) - nbateries + 1
+    maxvalue = max(digits[:offset])
+    index = digits.index(maxvalue, 0, offset)
+
+    if len(digits) == nbateries:
+        return "".join(digits)
+
+    if nbateries == 1:
+        return str(maxvalue)
+
+    return str(maxvalue) + maxbateryjoltage(digits[index+1:], nbateries-1)
+
+if __name__ == "__main__":
+    data = read_file_to_list("input.txt")
+    print(joltage(data))
---
+++ 2025/adventofcode3/test_adventofcode3.py
@@ -0,0 +1,20 @@
+import pytest
+
+from .adventofcode3 import *
+
+test_data = [
+    987654321111111,
+    811111111111119,
+    234234234234278,
+    818181911112111,
+]
+
+def test_adventofcode3():
+    result = joltage(test_data)
+    assert result == 3121910778619
+
+def test_max_joltage_per_battery():
+    assert maxbateryjoltage(list(str(987654321111111))) == '987654321111'
+    assert maxbateryjoltage(list(str(811111111111119))) == '811111111119'
+    assert maxbateryjoltage(list(str(234234234234278))) == '434234234278'
+    assert maxbateryjoltage(list(str(818181911112111))) == '888911112111'
