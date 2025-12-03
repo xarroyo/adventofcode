c1edc305fdeff01bc1c48da7df80c12f361ef419
Xavi Arroyo <xavier.arroyo@biteright.dental>
2025-12-03 13:23:23 +0100

    Setup

--- 2025/adventofcode3/adventofcode3.py
+++ 2025/adventofcode3/adventofcode3.py
@@ -1,7 +1,5 @@
+from infrastructue.files import read_file_to_list
 
-def read_file_to_list(filename):
-    with open(filename, "r", encoding="utf-8") as file:
-        return [int(line.strip()) for line in file]
 
 def joltage(data):
     return sum(int(maxbateryjoltage(list(str(v)))) for v in data)
@@ -20,5 +18,5 @@ def maxbateryjoltage(digits, nbateries=12):
     return str(maxvalue) + maxbateryjoltage(digits[index+1:], nbateries-1)
 
 if __name__ == "__main__":
-    data = read_file_to_list("input.txt")
+    data = read_file_to_list("2025/adventofcode3/input.txt")
     print(joltage(data))
---
+++ infrastructue/files.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+
+def read_file_to_list(filename):
+    # Path relative to project root (2 levels up from this file)
+    project_root = Path(__file__).parent.parent
+    input_file = project_root / filename
+    with open(input_file, "r", encoding="utf-8") as file:
+        return [int(line.strip()) for line in file]
